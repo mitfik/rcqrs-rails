4e9afb8fd3e68a50edc540abe97e6013b07bc28f
Robert Mitwicki <robert.mitwicki@fxitech.com>
2012-02-17 15:09:51 +0100

    Change install generators to put all config to config/initializer/rcqrs.rb, add Rqcrsq::Setting example to initializer

--- lib/generators/rcqrs/install/install_generator.rb
+++ lib/generators/rcqrs/install/install_generator.rb
@@ -26,9 +26,14 @@ ActiveSupport::Dependencies.autoload_paths += %W(
   #\{Rails.root.to_s\}/app/events
   #\{Rails.root.to_s\}/app/events/handlers
 )
+
+# Rcqrs::Setting.set |setting|
+#   setting.default_orm = :data_mapper # You can also use :active_record, :in_memory, :data_mapper
+#   setting.default_database_file_path = "config/event_storage.yml"
+# end
 CODE
 
-        initializer 'rcqrs_autoload_paths.rb', @snippet
+        initializer 'rcqrs.rb', @snippet
       end
 
       private
